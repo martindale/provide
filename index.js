2abedad508e04721fced0c871dc95228c6b11ff6
Eric Martindale <eric@ericmartindale.com>
2014-07-15 19:50:52 +0000

    Fix undefined data issue.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 module.exports = function(req, res, next) {
   res.provide = function( template , data ) {
+    if (!data) { var data = data; }
+
     res.format({
       json: function() { res.send( data[ template ] ); },
       html: function() { res.render( template , data ); }
